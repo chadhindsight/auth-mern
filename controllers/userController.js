fda8c0763f59b4e15d545ad1ef3f7146d71245a8
chad hinds <chadrickj8@gmail.com>
2024-03-26 15:44:54 -0400

    Import and use controllers needed for user authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,6 +37,25 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 });
 
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+});
+
 // Logout a user
 // @route   POST /api/users/logout
 // @access  Public
@@ -48,8 +67,35 @@ const logoutUser = (req, res) => {
     res.status(200).json({ message: 'Logged out successfully' });
 };
 
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+        });
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+});
 export {
     authUser,
     registerUser,
-    logoutUser
+    logoutUser,
+    updateUserProfile,
+    getUserProfile
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import { authUser } from '../controllers/userController.js';
+
+import {
+    authUser,
+    registerUser,
+    logoutUser,
+    updateUserProfile
+} from '../controllers/userController.js';
+
 const router = express.Router();
 
 // "api/users" is already connected to this file so we only need '/auth'
 router.post('/auth', authUser);
+router.post()
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 export default router;
