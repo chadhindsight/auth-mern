4f322df106801df05a163b73bdba935bda2641a0
chad hinds <chadrickj8@gmail.com>
2024-03-26 15:37:32 -0400

    Add functionality to register a new user and logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,51 @@
-const authUser = ((req, res) => {
+import asyncHandler from 'express-async-handler';
+
+const authUser = asyncHandler(async((req, res) => {
     res.status(200).json({ message: "Auth User ting" })
-})
+}))
+
+// Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    if (user) {
+        generateToken(res, user._id);
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+});
+
+// Logout a user
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = (req, res) => {
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+    res.status(200).json({ message: 'Logged out successfully' });
+};
 
 export { authUser }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 const PORT = process.env.PORT || 5001;
 
@@ -11,5 +12,9 @@ app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
     res.send("Server is up and running!")
 })
+// middlewares
+app.use(notFound)
+app.use(errorHandler)
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
