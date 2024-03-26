d1b3b42f4911f45de6770166ea054491de046680
chad hinds <chadrickj8@gmail.com>
2024-03-26 15:20:34 -0400

    setup access to endpoints related to "/api/users"

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import { authUser } from '../controllers/userController.js';
 const router = express.Router();
 
 // "api/users" is already connected to this file so we only need '/auth'
-router.post('/auth');
+router.post('/auth', authUser);
 
 export default router;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import userRoutes from './routes/userRoutes';
+
 const PORT = process.env.PORT || 5001;
 
 const app = express();
+
+// Allows use access to endpoints related to "/api/users"
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.send("Server is up and running!")
