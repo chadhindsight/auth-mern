5cd66b0eae21c876db7cd613e32d2b501d4fa5bf
chad hinds <chadrickj8@gmail.com>
2024-03-29 13:26:13 -0400

    Create a Furniture Schema

--- models/furnitureModel.js
+++ models/furnitureModel.js
@@ -1,1 +1,24 @@
 import mongoose from 'mongoose';
+
+const FurnitureSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 1
+    }
+}, {
+    timestamps: true
+})
+
+const Furniture = mongoose.model('Furniture', FurnitureSchema)
+
+export default Furniture
