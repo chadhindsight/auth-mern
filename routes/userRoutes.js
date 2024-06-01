290baac9f39b1b739265c8eeb7c3490975d95592
chad hinds <chadrickj8@gmail.com>
2024-06-01 11:24:59 -0400

    use single quotes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,7 +13,7 @@ const router = express.Router();
 // "api/users" is already connected to this file so we only need '/auth'
 router.post('/auth', authUser);
 router.post('/', registerUser);
-router.post("/logout", logoutUser)
+router.post('/logout', logoutUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
