c7fe86f1347d715d72b4f313d966c16df501db70
chad hinds <chadrickj8@gmail.com>
2024-06-03 14:13:04 -0400

    Add related CRUD functions to each related furniture endpoint

--- controllers/furnitureController.js
+++ controllers/furnitureController.js
@@ -3,16 +3,17 @@ import Furniture from '../models/furnitureModel.js';
 
 // @route   POST /api/furniture
 // @access  Private
-const addFurniture = asyncHandler(async () => {
+const addFurniture = asyncHandler(async (req, res) => {
     const { name, price, quantity } = req.body
 
     // Function to create a case-insensitive regex query
     const createRegexQuery = (input) => {
         const lowercaseInput = input.toLowerCase();
-        return { $regex: new RegExp('^' + lowercaseInput + '$', 'i') };
+        return new RegExp('^' + lowercaseInput + '$', 'i');
     };
 
-    const existingFurniture = await FurnitureModel.findOne({ name: createRegexQuery(name) });
+
+    const existingFurniture = await Furniture.findOne({ name: createRegexQuery(name) });
 
     if (!existingFurniture) {
 
@@ -23,6 +24,7 @@ const addFurniture = asyncHandler(async () => {
         }
 
         Furniture.create(furniture)
+        res.status(200).json({ message: 'Item successfully added!' });
     }
 
     else {
@@ -30,10 +32,12 @@ const addFurniture = asyncHandler(async () => {
     }
 
 })
-// @route   GET /api/furniture
+
+// @route   GET /api/furniture/:id
 // @access  Private
-const getFurniture = asyncHandler(async () => {
-    const furniture = Furniture.findByID({ id: req.params._id });
+const getFurniture = asyncHandler(async (req, res) => {
+    const furniture = await Furniture.findById(req.params.id);
+    console.log(req.params.id)
 
     if (furniture) {
         res.json(furniture)
@@ -45,21 +49,20 @@ const getFurniture = asyncHandler(async () => {
 
 // @route   GET /api/furniture
 // @access  Private
-const getListOfFurniture = asyncHandler(async () => {
-    const furnitureList = Furniture.find({});
+const getListOfFurniture = asyncHandler(async (req, res) => {
+    const listOfFurniture = await Furniture.find({});
 
-    if (furnitureList) {
-        res.json(furnitureList)
+    if (listOfFurniture.length) {
+        res.json(listOfFurniture);
     } else {
-        res.status(401);
-        throw new Error('Sorry, our inventory is currently empty.');
+        res.status(404).json({ message: 'Sorry, our inventory is currently empty.' });
     }
-})
+});
 
 // @route   PUT /api/furniture
 // @access  Private
 const updateFurniture = asyncHandler(async (req, res) => {
-    const furniture = await Furniture.findById(req.furniture._id);
+    const furniture = await Furniture.findById(req.params.id);
 
     if (furniture) {
         furniture.name = req.body.name || furniture.name;
@@ -67,10 +70,10 @@ const updateFurniture = asyncHandler(async (req, res) => {
         furniture.quantity = req.body.quantity || furniture.quantity;
 
 
-        const furniture = await Furniture.save();
+        const updatedFurniture = await furniture.save();
 
         res.json({
-            _id: furniture._id,
+            id: furniture._id,
             name: furniture.name,
             email: furniture.email,
         });
@@ -83,7 +86,7 @@ const updateFurniture = asyncHandler(async (req, res) => {
 // @route  DELETE / api/furniture/id
 // @access Private
 const deleteFurniture = asyncHandler(async (req, res) => {
-    const furnitureID = req.params.id;
+    const furnitureID = req.params._id;
 
     if (furnitureID) {
         Furniture.deleteOne({ id: req.params._id })
