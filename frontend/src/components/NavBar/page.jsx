709472e10b1728d1bb86504fc4e7b4e94ac41eba
chad hinds <chadrickj8@gmail.com>
2024-06-12 12:50:59 -0400

    Use section tag instead of div

--- frontend/src/components/NavBar/page.jsx
+++ frontend/src/components/NavBar/page.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import React from "react";
-import styles from "./NavBar.module.css"; // Import the CSS module
+import styles from "./NavBar.module.css";
 
 const NavAndHeader = () => {
   return (
     <nav className={styles.navBar}>
       <h1>Furniture</h1>
-      <div className={styles.navigation}>
+      <section className={styles.navigation}>
         <p className={styles.highlight}>Just In</p>
         <p>Furniture</p>
         <p>Rest</p>
@@ -14,7 +14,7 @@ const NavAndHeader = () => {
         <p>Outdoor</p>
         <p>Magazine</p>
         <p>Outlet</p>
-      </div>
+      </section>
       <div className={styles.icons}>
         <Image src="/images/user.png" width={25} height={24} />
         <Image src="/images/search.png" width={25} height={24} />
