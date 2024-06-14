c684c6f455d42862a1aaf5763f861ea243f51c5c
chad hinds <chadrickj8@gmail.com>
2024-06-14 12:45:59 -0400

    Leave another placeholder comment as a reminder of what to work on next

---
+++ frontend/src/app/users/page.jsx
@@ -0,0 +1,2 @@
+//NB: when a user clicks on the little profile icon, direct them to the sign/signup form, or, if authenticated, show them their profile.
+// Both happen on the "/profile" route. maybe use conditional rendering
