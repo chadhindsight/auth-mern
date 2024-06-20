e50eaa94265dd68234829f909230dadcad123695
chad hinds <chadrickj8@gmail.com>
2024-06-20 11:31:30 -0400

    Remove card

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,10 @@
 import HeroGrid from "@/components/HeroGrid/HeroGrid";
 import RootLayout from "./layout";
-import ProfileCard from "@/components/ProfileCard/ProfileCard";
 
 export default function Home() {
   return (
     <RootLayout>
       <main>
-        <ProfileCard />
         <HeroGrid />
       </main>
     </RootLayout>
