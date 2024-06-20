0446f3d1c0c75535ed7a7b7552c08ede16d7a5d7
chad hinds <chadrickj8@gmail.com>
2024-06-20 14:47:04 -0400

    remove unused import

--- frontend/src/components/ProfileCard/ProfileCard.jsx
+++ frontend/src/components/ProfileCard/ProfileCard.jsx
@@ -1,41 +1,68 @@
 "use client";
 import React, { useState } from "react";
 import styles from "./ProfileCard.module.css";
+import { useContext, useState } from "react";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import { AuthContext } from "@/app/context/authContext";
 
 const ProfileCard = () => {
-  // NB: see example use case in /auth page
-  const [formType, setFormType] = useState(null);
   // TODO: context, updateUser*
 
-  //TO updateUser, you can use the same form defined in auth,
-  // FLow: a user clicks on the edit button which brings up a form where they can change their user name
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const { updateUserProfile, user } = useContext(AuthContext);
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault();
+    // FLow: a user clicks on the edit button which brings up a form where they can change their user name
+    //TO updateUser, you can use the same form defined in auth,
+    try {
+      await updateUserProfile({ email, password });
+      setIsEditing(!setIsEditing);
+    } catch (error) {
+      console.error("Error updating profile:", error);
+    }
+  };
 
   return (
     <section className={styles.card}>
-      <button className={styles.editIcon}>
-        <FontAwesomeIcon icon={faPencilAlt} />
-      </button>
-      <header className={styles.header}>
-        <figure className={styles.picture}>
-          <img src="profile-picture.jpg" alt="Profile Picture" />
-        </figure>
-        <div className={styles.info}>
-          <h2>Furniture Associate</h2>
-          <p>Lorem Ipsum Deez</p>
-        </div>
-      </header>
-      <nav className={styles.actions}>
-        <a href="#" className={styles.actionLink}>
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-        <a href="#" className={styles.actionLink}>
-          <FontAwesomeIcon icon={faEnvelope} />
-        </a>
-        <button className={styles.logoutBtn}>Logout</button>
-      </nav>
+      {isEditing ? (
+        <Form
+          onSubmit={handleSubmit}
+          formType="edit"
+          styles={styles}
+          initialEmail={user.email}
+        />
+      ) : (
+        <>
+          <button className={styles.editIcon}>
+            <FontAwesomeIcon icon={faPencilAlt} onClick={handleEditClick} />
+          </button>
+          <header className={styles.header}>
+            <figure className={styles.picture}>
+              <img src="profile-picture.jpg" alt="Profile Picture" />
+            </figure>
+            <div className={styles.info}>
+              <h2>Furniture Associate</h2>
+              <p>Lorem Ipsum Deez</p>
+            </div>
+          </header>
+          <nav className={styles.actions}>
+            <a href="#" className={styles.actionLink}>
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href="#" className={styles.actionLink}>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+            <button className={styles.logoutBtn}>Logout</button>
+          </nav>
+        </>
+      )}
     </section>
   );
 };
